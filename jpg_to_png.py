5ba1e3be4b2b306924e3ad672c85ab3e1fccfd75
Clarsen D'Artois <116375601+clarsendartois@users.noreply.github.com>
2023-11-15 20:12:07 -0400

    new file, modified

--- jpg_to_png.py
+++ jpg_to_png.py
@@ -3,7 +3,6 @@
 # It will create a new folder
 # and put your PNGs in this folder.
 
-
 import sys
 import os
 from PIL import Image
@@ -15,7 +14,7 @@ if not os.path.exists(directory):
     os.makedirs(directory)
 
 for filename in os.listdir(path):
-  clean_name = os.path.splitext(filename)[0]
-  img = Image.open(f'{path}{filename}')
-  img.save(f'{directory}/{clean_name}.png', 'png')
-  print('all done!!')
+    clean_name = os.path.splitext(filename)[0]
+    img = Image.open(f'{path}{filename}')
+    img.save(f'{directory}/{clean_name}.png', 'png')
+    print('all done!!')
